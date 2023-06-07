5f4e21e2ba6b590c3b12c757d79aea5355ef5834
Eric Menezes <eric_menezes@live.com>
2023-06-07 20:17:01 -0300

    feat: configure openai gpt-3 for usage with nodejs

--- gpt3-commit.js
+++ gpt3-commit.js
@@ -1,65 +1,73 @@
-#!/usr/bin/env nod
+#!/usr/bin/env node
 
-import fs from 'fs';
-import openai from 'openai';
-import prompts from 'prompts';
-import dotenv from 'dotenv';
-import { promisify } from 'util';
-import { exec as originalExec } from 'child_process';
+const fs = require('fs');
+const prompts = require('prompts');
+const dotenv = require('dotenv');
+const { promisify } = require('util');
+const { exec: originalExec } = require('child_process');
+const { Configuration, OpenAIApi } = require('openai');
 
 dotenv.config();
 
-openai.apiKey = process.env.OPENAI_API_KEY;
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
+const openai = new OpenAIApi(configuration);
 const exec = promisify(originalExec);
 
 async function getGitSummary() {
-  try {
-    const { stdout } = await exec('git diff --cached --stat');
-    const summary = stdout.trim();
+    try {
+        const { stdout } = await exec('git diff --staged');
+        const summary = stdout.trim();
 
-    if (summary.length === 0) {
-      return null;
+        if (summary.length === 0) {
+            return null;
+        }
+
+        return summary;
+    } catch (error) {
+        console.error('Error while summarizing Git changes:', error);
+        process.exit(1);
     }
-
-    return summary;
-  } catch (error) {
-    console.error('Error while summarizing Git changes:', error);
-    process.exit(1);
-  }
 }
 
 (async () => {
-  const gitSummary = await getGitSummary();
+    const gitSummary = await getGitSummary();
+    if (!gitSummary) {
+        console.log('No changes to commit. Commit canceled.');
+        process.exit(0);
+    }
 
-  if (!gitSummary) {
-    console.log('No changes to commit. Commit canceled.');
-    process.exit(0);
-  }
+    const prompt = `Generate a Git commit message, respecting the Conventional Commits specifications, based on the following summary generate by 'git diff --staged' command: ${gitSummary}\n\nCommit message: `;
 
-  const prompt = `Generate a Git commit message based on the following summary: ${gitSummary}\n\nCommit message: `;
-  const response = await openai.Completion.create({
-    engine: 'davinci-codex',
-    prompt: prompt,
-    max_tokens: 50,
-    n: 1,
-    stop: null,
-    temperature: 0.5,
-  });
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+          });
 
-  const message = response.choices[0].text.trim();
+        const message = completion.data.choices[0].text.trim();
 
-  const confirm = await prompts({
-    type: 'confirm',
-    name: 'value',
-    message: `Suggested commit message: "${message}". Do you want to use it?`,
-    initial: true,
-  });
+        const confirm = await prompts({
+            type: 'confirm',
+            name: 'value',
+            message: `Suggested commit message: "${message}". Do you want to use it?`,
+            initial: true,
+        });
 
-  if (confirm.value) {
-    require('child_process').execSync(`git commit -m "${message}"`);
-    console.log('Committed with the suggested message.');
-  } else {
-    console.log('Commit canceled.');
-  }
+        if (confirm.value) {
+            require('child_process').execSync(`git commit -m "${message}"`);
+            console.log('Committed with the suggested message.');
+        } else {
+            console.log('Commit canceled.');
+        }
+    } catch (error) {
+        try {
+            console.error('Error occurred during AI completion:', error.response.data.error);
+        } catch (other_error) {
+            console.error('Error occurred during AI completion:', error);
+        }
+        process.exit(1);
+    }
 })();
